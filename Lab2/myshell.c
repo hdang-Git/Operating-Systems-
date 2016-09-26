59b2244f4589cb2ca2adc27ee85058dc523c6ab1
hdang-Git <hdang0010@gmail.com>
2016-09-26 01:46:47 -0400

    Added function check() to determine if built-in or background. Added launch() function to execute non-built-in commands. Odd behavior in strcmp where getline takes in \n so comparison is working now but might have to refactor to scanf to do away with it completely.

--- Lab2/myshell.c
+++ Lab2/myshell.c
@@ -7,47 +7,61 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <string.h>
+#include <sys/wait.h>
+
+//function prototypes
 void parseCmd(char*, char*[]);
-char* trim(char*);
+char* trim(char*, char);
 int countSpace(char*,char);
-char* check(char*, int*, int*, int*, int*, char**);
+void launch(pid_t, char*[]);
+void check(char*[], int, int*, int*, char*[]);
 
 int main()
 {
   char* inputCmd = NULL;
   int bytesRead; //# of bytes read from getline()
-  //int argc;
-  //char* argv[100];
   size_t size = 100;
   
   char cwd[1024];
-  char* built_in[] = {"ls", "exit", "cd", "ls", "help"}; // list of commands
-  //char* cmd;  
+  char* built_in[] = {"ls", "exit", "cd", "ls", "help", "\0"}; // list of commands
+
   int valid = FALSE;			//valid command
   int bg = FALSE;				//background process
   //int l_bracket = FALSE;		//stdin redirection
   //int r_bracket = FALSE;        //stdout redirection
-
+  pid_t pid;
   while(TRUE){
 	  getcwd(cwd, sizeof(cwd));         //get current working directory 
       printf("MyShell~%s: ", cwd);  
       
       bytesRead = getline(&inputCmd, &size, stdin); //get user input and the error code
       if(bytesRead == -1){
-          printf("Error! %d\n", bytesRead);
+          printf("Error! %d\n", bytesRead);	
       } else {                                      //user input scanner successful
-          printf("Before: %s\n", inputCmd);
-          inputCmd = trim(inputCmd);
-          printf("After: %s\n", inputCmd);
+          //printf("Before trim: %s\n", inputCmd);
+          inputCmd = trim(inputCmd, ' ');
+          //printf("After trim : %s\n", inputCmd);
           int a = countSpace(inputCmd, ' ');
           char* cmd[a];
-          printf("%d \n", a);
+          printf("size of input cmd array%d \n", a);
           parseCmd(inputCmd,cmd);
+      	
+      	  check(cmd, a, &valid, &bg, built_in);
+      	  if(valid){							//valid built_in cmd
+      	  	printf("I'm VALID\n");
+      	  	valid = FALSE;		//flip the flag to false for next read
+      	  	//call switch command
+      	  } else {								//not a built_in cmd
+      	  	printf("I'm INVALID\n");
+      	  	launch(pid, cmd);
+      	  	//fork the process and let the unix system handle it
+      	  }
+      	     
       }
    }
 }
 
-char* trim(char* input){
+char* trim(char* input, char delim){
 	//printf("initial length: %d\n", (int)strlen(input));
 	//remove leading whitespace
 	while(!isalnum(*input)){		//while first character is alphanumeric     
@@ -59,7 +73,7 @@ char* trim(char* input){
 	int length = strlen(input);
 	//printf("length: %d\n", length);
 	char* str = input + length - 1;
-	while(length > 0 && !isalnum(*str)){
+	while(length > 0 && *input == delim){
 		str--;
 	}
 	*(str+1) = '\0';
@@ -94,14 +108,51 @@ void parseCmd(char* input, char* arr[]){
    printf("\n");
 }
 
-
-char* check(char* input, int* valid, int* bg, int* left, int* right, char** cmd){
-	printf("valid: %d\n background: %d\n left %d\n right %d\n ", *valid, *bg, *left, *right);
-    //if the first argment is in the list of commands
-        //if second argument is ampersand
-    // if anywhere there is a redirection for every odd argument (input/output)
+//NOTE: Behavior of strcmp(input[0], built_in[i]) == 0) 
+//the newline is included in input[0] with getline where comparison is
+// "cd\n" == "cd" fails because newline
+// "cd " == "cd" works, adding space pads away the \n into separate argument
+//TODO: revisit in case it causes problems in the future   
+void check(char* input[], int size, int* valid, int* bg, char* built_in[]){
+	printf("valid: %d\n background: %d\n", *valid, *bg);
+	printf("input cmd: %s\n", input[0]);
+    //if the first argument is in the list of commands
     int i;
+    for(i = 0; strcmp(built_in[i],"\0") != 0; i++){
+    	printf("i'm in the loop: %s\n", built_in[i]);
+    	if(strcmp(input[0], built_in[i]) == 0){
+    		*valid = TRUE;
+    		printf("condition is met\n");
+    	}
+    	printf("difference comparator: %d\n", strcmp(input[0], built_in[i]));
+    } 
+    printf("valid %d\n", *valid);
+    //if last argument is ampersand
+    if(*(input[size-1]) == '&'){
+    	*bg = TRUE;
+    } 
+    printf("last character: %s\n", input[size-1]);
+    printf("background: %d\n", *bg);
+    // if anywhere there is a redirection for every odd argument (input/output)
+    
    
+}
+
+void launch(pid_t pid, char* args[]){
+	pid = fork();									//fork the current process
+	if(pid == 0){
+		printf("Child process with id %d. My parent is %d. \n", getpid(),  getppid());
+		execlp(args[0], *args, NULL);
+		exit(0);
+	} else if(pid < 0){								//Error handling if error code
+		perror("Error incorrect arguments\n");		//Print error message
+        exit(-1);									//Indicate unsuccessful program termination
+	}else {
+	    printf("Parent process with id %d my child is %d. \n", getpid(),  pid);
+		//if not background
+		//if background
+		waitpid(pid, NULL, 0);						//Wait for child process to finish
+	}
 }
 
 
